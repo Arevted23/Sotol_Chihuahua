2fc5a95a6c476abbd05af9f53fffcef90a0aa9ae
Hilario Arevalo <hilarioarevalo03@gmail.com>
2024-10-05 23:05:55 -0600

    home update to locals and new structure

---
+++ src/app/shared/pages/home/home_locales_component.ts
@@ -0,0 +1,27 @@
+export const HomeLocales = {
+  localTitleHome: 'Hacienda de chihuahua',
+  localSectionTitleTwo: 'El secreto mejor guardado del desierto Chihuahuense',
+  localBtnSectionDescription: 'Conoce Nuestra Historia',
+  localSectionTitleThree: 'Mixologia',
+  localDescriptionMixology:
+    'Descubre, prepara y disfruta de la mixologia que tenemos para ti con nuestro sotol de hacienda chihuahua.',
+  localLinkMixSectionThree: 'Mixologia',
+  localKnowProductLinks: 'Conoce los productos',
+};
+
+export const HomeStyles = {
+  localStyleHeaderTag: 'bg-Home text-center uppercase text-white',
+  localStyleArticleTag: 'bghome p-10 font-medium text-4xl sm:text-9xl',
+  localStyleSectionTag:
+    'bgSecret p-10 font-medium text-2xl sm:text-6xl pl-12 pr-12',
+  localStyleH2tag: 'text-black m-0 sm:ml-16 sm:mr-16',
+  localStyleAnchorTag: 'bg-black text-sm p-2 hover:cursor-pointer',
+  section:{
+    localStyleSectionMain: 'p-20 sm:p-40 bgMixologia grid gap-8 place-items-center sm:grid-cols-2',
+    localStyleH3tag: 'text-4xl sm:text-7xl',
+    localStyleArticleTag: 'grid gap-4 text-black sm:grid-cols-2',
+    localStyleDivTag: 'bg-white hover:transition-all hover:cursor-pointer hover:bg-black hover:text-white p-1'
+  }
+};
+
+//dividir y desestrcuturar los objetos, tanto en estilos como locales
