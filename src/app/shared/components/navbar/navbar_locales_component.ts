fbd78df17984d2d3862522490e5a2021718afe04
Hilario Arevalo <hilarioarevalo03@gmail.com>
2024-10-05 23:05:46 -0600

    navbar update to locals and new structure

---
+++ src/app/shared/components/navbar/navbar_locales_component.ts
@@ -0,0 +1,10 @@
+export const NavigationLocales = {
+  linkNavigationStart: 'Inicio',
+  linkNavigationHistory: 'Historia',
+  linkNavigationCertifications: 'Certificaciones',
+  linkNavigationProducts: 'Productos',
+  linkNavigationMixology: 'Mixologia',
+  linkNavigationContact: 'Contacto',
+  linkNavigationShopStore: 'Comprar Ahora',
+
+}
